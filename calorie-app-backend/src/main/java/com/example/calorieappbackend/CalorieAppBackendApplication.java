e6eedf779edcf55e4e64d00c4e4e5129dc3d1f48
sajeerahamed15 <ahamedsajeer.15@cse.mrt.ac.lk>
2022-05-14 11:44:11 +0100

    Init backend app

---
+++ calorie-app-backend/src/main/java/com/example/calorieappbackend/CalorieAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.calorieappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalorieAppBackendApplication.class, args);
+	}
+
+}
---
+++ calorie-app-backend/src/test/java/com/example/calorieappbackend/CalorieAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calorieappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorieAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
